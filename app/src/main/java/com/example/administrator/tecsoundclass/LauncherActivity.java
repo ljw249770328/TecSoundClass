18629a38cb96e289f440677d3db9c5e5e62d86f5
ljw249770328 <44216222+ljw249770328@users.noreply.github.com>
2018-12-09 23:59:50 +0800

    gitpal创建数据库完成

--- app/src/main/java/com/example/administrator/tecsoundclass/LauncherActivity.java
+++ app/src/main/java/com/example/administrator/tecsoundclass/LauncherActivity.java
@@ -8,13 +8,15 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
+import org.litepal.LitePal;
+
 public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
-
+        LitePal.getDatabase();
         Handler handler = new Handler(){
             @Override
             public void handleMessage(Message msg) {
@@ -23,7 +25,7 @@ public class LauncherActivity extends AppCompatActivity {
                 finish();
             }
         };
-        handler.sendEmptyMessageDelayed(1,2300);//延时发送，第二个参数跟的毫秒
+        handler.sendEmptyMessageDelayed(1,2000);//延时发送，第二个参数跟的毫秒
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
