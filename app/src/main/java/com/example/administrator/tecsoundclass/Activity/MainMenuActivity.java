efd11a8726e4e9c9afd88fbe3cfa9177df62b15d
ljw249770328 <44216222+ljw249770328@users.noreply.github.com>
2019-03-12 00:26:49 +0800

    已解决数据库查询类型问题,下一步开始在mainactivity中获取登录的用户信息

--- app/src/main/java/com/example/administrator/tecsoundclass/Activity/MainMenuActivity.java
+++ app/src/main/java/com/example/administrator/tecsoundclass/Activity/MainMenuActivity.java
@@ -71,7 +71,7 @@ public class MainMenuActivity extends AppCompatActivity {
         mRgTab=findViewById(R.id.rg_main);
         StudentID=getIntent().getExtras().getString("LoginId");
         //查询User信息
-        UInfoRequest(StudentID);
+//        UInfoRequest(StudentID);
         Toast.makeText(MainMenuActivity.this,"欢迎 "+user.getUser_name(),Toast.LENGTH_SHORT).show();
         //点击切换Fragment
         mRgTab.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -123,53 +123,55 @@ public class MainMenuActivity extends AppCompatActivity {
         }
         ft.commit();
     }
-    private void UInfoRequest(final String accountNumber) {
-        //请求地址
-        String url = "http://101.132.71.111:8080/TecSoundWebApp/GetUInfoServlet";
-        String tag = "getinfo";
-        //取得请求队列
-        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-
-        //防止重复请求，所以先取消tag标识的请求队列
-        requestQueue.cancelAll(tag);
-
-        //创建StringRequest，定义字符串请求的请求方式为POST(省略第一个参数会默认为GET方式)
-        final StringRequest request = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonObject = (JSONObject) new JSONObject(response).get("params");  //注③
-                            user.setUser_age(jsonObject.getString("user_age"));
-                            user.setUser_identity(jsonObject.getString("user_identity"));
-                            user.setUser_sex(jsonObject.getString("user_sex"));
-                            user.setUser_name(jsonObject.getString("user_name"));
-                            user.setUser_pic_src(jsonObject.getString("user_pic_src"));
-                        } catch (JSONException e) {
-                            //做自己的请求异常操作，如Toast提示（“无网络连接”等）
-                            Log.e("TAG", e.getMessage(), e);
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //做自己的响应错误操作，如Toast提示（“请稍后重试”等）
-                Log.e("TAG", error.getMessage(), error);
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                params.put("user_id", accountNumber);  //注⑥
-                return params;
-            }
-        };
-
-        //设置Tag标签
-        request.setTag(tag);
-
-        //将请求添加到队列中
-        requestQueue.add(request);
-
-    }
+//    private void UInfoRequest(final String accountNumber) {
+//        //请求地址
+//        String url = "http://101.132.71.111:8080/TecSoundWebApp/GetUInfoServlet";
+//        String tag = "getinfo";
+//        //取得请求队列
+//        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+//
+//        //防止重复请求，所以先取消tag标识的请求队列
+//        requestQueue.cancelAll(tag);
+//
+//        //创建StringRequest，定义字符串请求的请求方式为POST(省略第一个参数会默认为GET方式)
+//        final StringRequest request = new StringRequest(Request.Method.POST, url,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        try {
+//                            JSONObject jsonObject = (JSONObject) new JSONObject(response).get("params");
+//
+//                            Toast.makeText(MainMenuActivity.this,jsonObject.toString(),Toast.LENGTH_SHORT).show();
+//                            user.setUser_age(jsonObject.getString("user_age"));
+//                            user.setUser_identity(jsonObject.getString("user_identity"));
+//                            user.setUser_sex(jsonObject.getString("user_sex"));
+//                            user.setUser_name(jsonObject.getString("user_name"));
+//                            user.setUser_pic_src(jsonObject.getString("user_pic_src"));
+//                        } catch (JSONException e) {
+//                            //做自己的请求异常操作，如Toast提示（“无网络连接”等）
+//                            Log.e("TAG", e.getMessage(), e);
+//                        }
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                //做自己的响应错误操作，如Toast提示（“请稍后重试”等）
+//                Log.e("TAG", error.getMessage(), error);
+//            }
+//        }) {
+//            @Override
+//            protected Map<String, String> getParams() throws AuthFailureError {
+//                Map<String, String> params = new HashMap<>();
+//                params.put("user_id", accountNumber);  //注⑥
+//                return params;
+//            }
+//        };
+//
+//        //设置Tag标签
+//        request.setTag(tag);
+//
+//        //将请求添加到队列中
+//        requestQueue.add(request);
+//
+//    }
 }
