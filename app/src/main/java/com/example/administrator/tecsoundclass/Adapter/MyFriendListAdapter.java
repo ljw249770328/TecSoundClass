5ec340e3b4402203ed9a20241a0b631647b1f0f5
ljw249770328 <44216222+ljw249770328@users.noreply.github.com>
2019-05-06 22:10:26 +0800

    friendfragment基础搭建完成

--- app/src/main/java/com/example/administrator/tecsoundclass/Adapter/MyFriendListAdapter.java
+++ app/src/main/java/com/example/administrator/tecsoundclass/Adapter/MyFriendListAdapter.java
@@ -1,38 +1,27 @@
 package com.example.administrator.tecsoundclass.Adapter;
 
 import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.Adapter;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.administrator.tecsoundclass.Activity.LoginActivity;
-import com.example.administrator.tecsoundclass.Activity.MainMenuActivity;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.signature.ObjectKey;
 import com.example.administrator.tecsoundclass.JavaBean.Follow;
 import com.example.administrator.tecsoundclass.JavaBean.User;
 import com.example.administrator.tecsoundclass.R;
-import com.example.administrator.tecsoundclass.utils.VolleyCallback;
-import com.liulishuo.filedownloader.message.MessageSnapshotFlow;
+import com.example.administrator.tecsoundclass.utils.TransferMore;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
-import java.util.Map;
 
 
 public class MyFriendListAdapter extends Adapter<MyFriendListAdapter.Viewholder> {
@@ -68,7 +57,7 @@ public class MyFriendListAdapter extends Adapter<MyFriendListAdapter.Viewholder>
     @Override
     public void onBindViewHolder(@NonNull final Viewholder viewholder, int i) {
         user_id=mFollowList.get(i).getFan_user_id();
-        Transfer2User.GetUserById(mContext,user_id,new Handler(){
+        TransferMore.GetUserById(mContext,user_id,new Handler(){
             @Override
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
@@ -76,6 +65,13 @@ public class MyFriendListAdapter extends Adapter<MyFriendListAdapter.Viewholder>
                     case 1:
                         User user = (User) msg.obj;
                         viewholder.mFriendName.setText(user.getUser_name());
+                        try {
+                            if (!user.getUser_pic_src().equals("")){
+                                Glide.with(mContext).load(new URL(user.getUser_pic_src())).signature(new ObjectKey(user.getUpdate_time())).encodeQuality(70).into(viewholder.mFriendPic);
+                            }
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        }
                 }
             }
         });
@@ -85,43 +81,5 @@ public class MyFriendListAdapter extends Adapter<MyFriendListAdapter.Viewholder>
     @Override
     public int getItemCount() {
         return mFollowList.size();
-    }
-    static class Transfer2User{
-        public static void GetUserById(final Context context, final String id, final Handler handler){
-            new Thread(){
-                @Override
-                public void run() {
-                    super.run();
-                    String url = "http://101.132.71.111:8080/TecSoundWebApp/GetUInfoServlet";
-                    Map<String, String> params = new HashMap<>();
-                    params.put("user_id",id);
-                    VolleyCallback.getJSONObject(context, "GetFollowuser", url, params, new VolleyCallback.VolleyJsonCallback() {
-                        @Override
-                        public void onFinish(JSONObject r) {
-                            try {
-                                JSONArray users=r.getJSONArray("users");
-                                JSONObject user= (JSONObject) users.get(0);
-
-                                User u=new User();
-                                //封装user对象
-                                u.setUser_id(user.getString("user_id"));
-                                u.setUser_age(user.getString("user_age"));
-                                u.setUser_identity(user.getString("user_identity"));
-                                u.setUser_sex(user.getString("user_sex"));
-                                u.setUser_name(user.getString("user_name"));
-                                u.setUser_pic_src(user.getString("user_pic_src"));
-                                u.setUpdate_time(user.getString("update_time"));
-                                Message message =new Message();
-                                message.what=1;
-                                message.obj=u;
-                                handler.sendMessage(message);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-                }
-            }.start();
-        }
     }
 }
